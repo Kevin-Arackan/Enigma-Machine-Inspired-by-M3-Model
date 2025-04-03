3ea08af8efeca42476c830101bbfbe43a519a477
Kevin Arackan <155002490+Kevin-Arackan@users.noreply.github.com>
2025-04-03 19:01:41 -0400

    Enhanced input

--- Enigma.java
+++ Enigma.java
@@ -6,19 +6,42 @@ public class Enigma {
     public static void main(String[] args) {
         // How to execute: java Enigma rotor1ID rotor2ID rotor3ID ringSetting plugboardSettings initialRotorPositions
         // Example: java Enigma V II I IHQ ET LD NP QS RA UW UJJ
+
+        // Reading rotor input
         Scanner reader = new Scanner(System.in);
-        String ringstellung = args[3];
-        Rotor rotor1 = new Rotor(args[0], ringstellung.charAt(0));
-        Rotor rotor2 = new Rotor(args[1], ringstellung.charAt(1));
-        Rotor rotor3 = new Rotor(args[2], ringstellung.charAt(2));
-        String plugboardSettings = args[4] + " " + args[5] + " " + args[6] + " " + args[7] + " " + args[8] + " " + args[9];
+        System.out.println("Enter Rotor 1:");
+        String r1 = reader.nextLine();
+        System.out.println("Enter Rotor 2:");
+        String r2 = reader.nextLine();
+        System.out.println("Enter Rotor 3:");
+        String r3 = reader.nextLine();
+
+        // Reading ring settings
+        System.out.println("Enter Ring settings:");
+        String ringstellung = reader.nextLine();
+
+        // Initializing rotors
+        Rotor rotor1 = new Rotor(r1, ringstellung.charAt(0));
+        Rotor rotor2 = new Rotor(r2, ringstellung.charAt(1));
+        Rotor rotor3 = new Rotor(r3, ringstellung.charAt(2));
+
+        // Reading plugboard settings
+        System.out.println("Enter plugboard settings:");
+        String plugboardSettings = reader.nextLine();
+
         char letter = 'A';
         Plugboard plugboard = new Plugboard(plugboardSettings);
         plugboard.adjustOutput(plugboardSettings);
         Reflector reflector = new Reflector("B");
-        rotor1.grundstellung(args[10].charAt(0));
-        rotor2.grundstellung(args[10].charAt(1));
-        rotor3.grundstellung(args[10].charAt(2));
+
+        // Reading rotor initial position
+        System.out.println("Enter grundstellung settings:");
+        String grundstellungSettings = reader.nextLine();
+        rotor1.grundstellung(grundstellungSettings.charAt(0));
+        rotor2.grundstellung(grundstellungSettings.charAt(1));
+        rotor3.grundstellung(grundstellungSettings.charAt(2));
+
+        // Encryption/decryption
         System.out.println("What do you want to encrypt or decrypt?");
         String input = reader.nextLine();
         StringBuilder output = new StringBuilder();
